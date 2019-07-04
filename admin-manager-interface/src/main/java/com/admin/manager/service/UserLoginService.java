4faa3c32259f8491275aa0092cc39d82887a90cf
yxdengj <18163581230@163.com>
2019-07-04 21:35:25 +0800

    增登出接口

--- admin-manager-interface/src/main/java/com/admin/manager/service/UserLoginService.java
+++ admin-manager-interface/src/main/java/com/admin/manager/service/UserLoginService.java
@@ -5,4 +5,6 @@ import com.admin.common.pojo.ResultData;
 public interface UserLoginService {
 	public ResultData login(String username, String password);
 	public Boolean isTokenExist (String token);
+	public ResultData getUserByToken(String token);
+	public ResultData logOut(String token);
 }
